31945ec645bdd184e6735a058e7d5f01ace0db15
Marsha Annon <marshaannon@gmail.com>
2020-03-06 16:59:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,10 @@ def reverse_each_word(array)
   new_array.each {|x| x.reverse!}
   return new_array.join(" ")
 end
+
+
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  new_array.collect {|x| x.reverse!}
+  return new_array.join(" ")
+end
